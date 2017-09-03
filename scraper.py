73aea479375f921f9fcc1ea4a8064852cb0a681c
Do Tran <trantdo@gmail.com>
2017-09-03 22:34:49 +0200

    Query each location consecutively and wait for table updated

--- scraper.py
+++ scraper.py
@@ -23,47 +23,52 @@ USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) " + \
 
 URL_LOGIN = "http://datalogger.capnuocnhabe.vn"
 # URL_LOGIN = "http://113.161.69.85:1802/Login.aspx"
-
+ACCOUNT = {'id': "sawaco", 'pw': "123456"}
 URL_LOGGER = "http://113.161.69.85:1802/Consumer/Logger/Daily_Monthly.aspx"
-START_DATE = '01/01/2017'
-END_DATE   = '18/01/2017'
+START_DATE = '01/09/2017'
+END_DATE   = '05/09/2017'
 XNTD = ["39 Ben Van Don",
         "65 BEN VAN DON",
-        "90 BEN VAN DON",
-        "BEN VAN DON_NGUYEN KHOAI",
-        "CALMETTE - BEN VAN DON",
-        "CAU HIEP PHUOC",
-        "CAU RACH ONG 1",
-        "Cau Rach Ong 2",
-        "CAU TAC BEN RO",
-        "CTY PT CN TAN THUAN",
-        "D1000 Huynh Tan Phat",
-        "D300 Cau Ba Chiem",
-        "D600 Cau Ong Lon",
-        "D600 NVLinh_NHTho",
-        "KCN HIEP PHUOC",
-        "KCX TThuan",
-        "LONG THOI - NHON DUC",
-        "NGUYEN BINH",
-        "NHTho_PHLau",
-        "NVLinh_NLBang",
+        # "90 BEN VAN DON",
+        # "BEN VAN DON_NGUYEN KHOAI",
+        # "CALMETTE - BEN VAN DON",
+        # "CAU HIEP PHUOC",
+        # "CAU RACH ONG 1",
+        # "Cau Rach Ong 2",
+        # "CAU TAC BEN RO",
+        # "CTY PT CN TAN THUAN",
+        # "D1000 Huynh Tan Phat",
+        # "D300 Cau Ba Chiem",
+        # "D600 Cau Ong Lon",
+        # "D600 NVLinh_NHTho",
+        # "KCN HIEP PHUOC",
+        # "KCX TThuan",
+        # "LONG THOI - NHON DUC",
+        # "NGUYEN BINH",
+        # "NHTho_PHLau",
+        # "NVLinh_NLBang",
         "VUON UOM BOO",
-        "Vuon uom Tan Thuan",
+        # "Vuon uom Tan Thuan",
         "D1000 Huynh Tan Phat (ÐC)",
-        "D300 Cau Ba Chiem (ÐC)",
-        "D600 NVLinh_NHTho (ÐC)",
-        "NHTho_PHLau (ÐC)",
-        "NVLinh_NLBang (ÐC)",
-        "Vuon uom BOO (ÐC)",
-        "Vuon uom Tan Thuan (ÐC)",
+        # "D300 Cau Ba Chiem (ÐC)",
+        # "D600 NVLinh_NHTho (ÐC)",
+        # "NHTho_PHLau (ÐC)",
+        # "NVLinh_NLBang (ÐC)",
+        # "Vuon uom BOO (ÐC)",
+        # "Vuon uom Tan Thuan (ÐC)",
 ]
 
+XNTD = ["102 LE QUOC HUNG"]
 
-def site_log_in(driver):
+
+def site_log_in(driver, account):
+    wait = WebDriverWait(driver, 10)
+    wait.until(EC.presence_of_element_located((By.ID, "ContentPlaceHolder1_ucLogin1_Login1_LoginButton")))
+
     # METHOD 1
     # --------
-    driver.find_element_by_xpath("//input[@id='ContentPlaceHolder1_ucLogin1_Login1_UserName']").send_keys("sawaco")
-    driver.find_element_by_xpath("//input[@id='ContentPlaceHolder1_ucLogin1_Login1_Password']").send_keys("123456")
+    driver.find_element_by_xpath("//input[@id='ContentPlaceHolder1_ucLogin1_Login1_UserName']").send_keys(account['id'])
+    driver.find_element_by_xpath("//input[@id='ContentPlaceHolder1_ucLogin1_Login1_Password']").send_keys(account['pw'])
     driver.find_element_by_xpath("//input[@id='ContentPlaceHolder1_ucLogin1_Login1_LoginButton']").click()
 
     # METHOD 2
@@ -89,27 +94,121 @@ def site_log_in(driver):
     # login_btn = driver.find_element_by_xpath("//input[@id='ContentPlaceHolder1_ucLogin1_Login1_LoginButton']")
     # login_btn.click()
 
+    wait.until(EC.presence_of_all_elements_located((By.ID, "RAD_SLIDING_PANE_TEXT_RadSlidingPane2")))
+    driver.maximize_window()
+
 
 def get_locations_list(driver):
     """Return the list of all locations/meters available in the dropdown.
     """
-    elem = driver.find_element_by_xpath("//input[@id='ctl00_ContentPlaceHolder1_ucDailyReportConsumer_cboSites_Input']")
-    elem.click()
-    elem.send_keys(Keys.ARROW_DOWN + Keys.ARROW_UP + Keys.ENTER)
-
+    elem = rewind_dropdown_list(driver, 1)
     locations = []
     while True:
-        val = driver.find_element_by_xpath(
-            "//input[@id='ctl00_ContentPlaceHolder1_ucDailyReportConsumer_cboSites_ClientState']")\
-            .get_attribute("value")
-        # print(val)
-        loc = val.split(',')[1].split(':')[1].strip('"')
+        loc = get_highlighted_location(driver)
         if locations and loc == locations[-1]:
             break
         locations.append(loc)
         elem.send_keys(Keys.ARROW_DOWN + Keys.ENTER)
-    print(locations)
+    rewind_dropdown_list(elem, len(locations))
     return locations
+
+
+def get_highlighted_location(driver):
+    """Read the name of the location/meter that are
+    currently highlighted in the dropdown list.
+
+    Example:
+    value = {"logEntries":[],"value":"Vuon uom Tan Thuan (ÐC)","text":"Vuon uom Tan Thuan (ĐC)","enabled":true,"checkedIndices":[],"checkedItemsTextOverflows":false}
+    """
+    value = driver.find_element_by_xpath(
+        "//input[@id='ctl00_ContentPlaceHolder1_ucDailyReportConsumer_cboSites_ClientState']") \
+        .get_attribute("value")
+    val = value.split(',')[1].split(':')[1].strip('"')
+    text = value.split(',')[2].split(':')[1].strip('"')
+    return {'value': val, 'text': text}
+
+
+def rewind_dropdown_list(driver, num_steps):
+    """Go to the beginning of the dropdown list.
+    """
+    element = driver.find_element_by_xpath(
+        "//input[@id='ctl00_ContentPlaceHolder1_ucDailyReportConsumer_cboSites_Input']")
+    element.click()
+    element.send_keys(Keys.ARROW_DOWN)
+    for i in range(num_steps):
+        element.send_keys(Keys.ARROW_UP)
+    element.send_keys(Keys.ENTER)
+    return element
+
+
+def select_a_location(driver, list_length, location):
+    """Select a location/meter from the dropdown list.
+    """
+    elem = rewind_dropdown_list(driver, list_length)
+    elem.click()
+
+    previous_loc = None
+    while True:
+        current_loc = get_highlighted_location(driver)
+        # print(current_loc)
+        if current_loc == previous_loc:
+            print("Couldn't find the requested location/meter.")
+            return False
+        if location in current_loc.values():
+            return True
+        previous_loc = current_loc
+        elem.send_keys(Keys.ARROW_DOWN + Keys.ENTER)
+
+
+class TableWait(object):
+    def __init__(self, driver, timeout=10):
+        self.driver = driver
+        self.timeout = timeout
+
+    def __enter__(self):
+        tables = self.driver.find_elements_by_xpath("//table[@id='ctl00_ContentPlaceHolder1_ucDailyReportConsumer_grv_ctl00']")
+        if len(tables) == 0:
+            self.begin = True
+        else:
+            self.begin = False
+            self.old_table = tables[0]
+
+        # Click on the Xem button
+        self.driver.find_element_by_xpath(
+            "//input[@id='ctl00_ContentPlaceHolder1_ucDailyReportConsumer_btnView_input']").click()
+
+    def __exit__(self, *_):
+        if self.begin:
+            # WebDriverWait(self.driver, self.timeout).until(
+            #     EC.element_to_be_clickable((By.ID, "ctl00_ContentPlaceHolder1_ucDailyReportConsumer_grv_ctl00")))
+            wait_for(self.table_has_shown, self.timeout)
+        else:
+            wait_for(self.table_has_updated, self.timeout)
+
+    def table_has_shown(self):
+        tables = self.driver.find_elements_by_xpath("//table[@id='ctl00_ContentPlaceHolder1_ucDailyReportConsumer_grv_ctl00']")
+        return len(tables) > 0
+
+    def table_has_updated(self):
+        new_table = self.driver.find_element_by_xpath("//table[@id='ctl00_ContentPlaceHolder1_ucDailyReportConsumer_grv_ctl00']")
+        return new_table.id != self.old_table.id
+
+
+def wait_for(condition_function, timeout):
+    start_time = time.time()
+    while time.time() < start_time + timeout:
+        if condition_function():
+            print("Waited successfully in {:.1f} sec.".format(time.time() - start_time))
+            return True
+        else:
+            time.sleep(0.1)
+    raise Exception("Timeout {} sec waiting for {}.".format(timeout, condition_function.__name__))
+    # print("  Timeout waiting for table update.")
+    # return False
+
+
+def scrape_table_per_location(driver):
+    print("Inside of scrape_table_per_location()")
 
 
 def main():
@@ -119,14 +218,11 @@ def main():
     driver = webdriver.PhantomJS(executable_path=PHANTOMJS_EXE, desired_capabilities=caps)
 
     driver.get(URL_LOGIN)
-    wait = WebDriverWait(driver, 10)
-    wait.until(EC.presence_of_element_located((By.ID, "ContentPlaceHolder1_ucLogin1_Login1_LoginButton")))
-    site_log_in(driver)
-    wait.until(EC.presence_of_all_elements_located((By.ID, "RAD_SLIDING_PANE_TEXT_RadSlidingPane2")))
-    driver.maximize_window()
+    site_log_in(driver, ACCOUNT)
 
     # Now that you are logged in, go to the page of interest
     driver.get(URL_LOGGER)
+    wait = WebDriverWait(driver, 10)
     wait.until(EC.element_to_be_clickable((By.ID, "ctl00_ContentPlaceHolder1_ucDailyReportConsumer_btnView_input")))
 
     element = driver.find_element_by_xpath("//input[@id='ctl00_ContentPlaceHolder1_ucDailyReportConsumer_dtmStart_dateInput']")
@@ -138,118 +234,30 @@ def main():
     element.send_keys(END_DATE)
 
     locations = get_locations_list(driver)
-    for i in locations:
-        print(i)
+    # for i in locations:
+    #     print(i)
 
+    for i, loc in enumerate(XNTD):
+        print("\nLocating '{}' ...".format(loc))
+        if loc not in [item['value'] for item in locations] and \
+                loc not in [item['text'] for item in locations]:
+            print("** '{}' not found.".format(loc))
+            continue
+        if select_a_location(driver, len(locations), loc):
+            print("OK.")
+            table_wait = TableWait(driver, timeout=30)
+            try:
+                with table_wait:
+                    pass
+            except Exception as msg:
+                print("** Omitting '{}': {}".format(str(loc), msg))
+                continue
+            driver.save_screenshot('screenshoot_{}_{}.png'.format(i, loc))
 
+            element = driver.find_element_by_xpath(
+                "//*[@id='ctl00_ContentPlaceHolder1_ucDailyReportConsumer_grv_ctl00__0']/td[2]")
+            print("  --> " + element.text)
 
-    # driver.find_element_by_xpath("//input[@id='ctl00_ContentPlaceHolder1_ucDailyReportConsumer_cboSites_Input']").click()
-    # driver.find_element_by_xpath("//input[@id='ctl00_ContentPlaceHolder1_ucDailyReportConsumer_cboSites_Input']").send_keys(Keys.ARROW_DOWN + Keys.ARROW_UP + Keys.ENTER)
-    #
-    # locations = []
-    # while True:
-    #     val = driver.find_element_by_xpath("//input[@id='ctl00_ContentPlaceHolder1_ucDailyReportConsumer_cboSites_ClientState']").get_attribute("value")
-    #     # print(val)
-    #     loc = val.split(',')[1].split(':')[1].strip('"')
-    #     if locations and loc == locations[-1]:
-    #         break
-    #     locations.append(loc)
-    #     driver.find_element_by_xpath("//input[@id='ctl00_ContentPlaceHolder1_ucDailyReportConsumer_cboSites_Input']").send_keys(Keys.ARROW_DOWN + Keys.ENTER)
-    # # print(locations)
-
-
-        # driver.save_screenshot('screen_shoot_1.png')
-        # driver.find_element_by_xpath("//input[@id='ctl00_ContentPlaceHolder1_ucDailyReportConsumer_cboSites_Input']").send_keys(Keys.ARROW_DOWN + Keys.ENTER)
-        # driver.save_screenshot('screen_shoot_2.png')
-        # driver.find_element_by_xpath("//input[@id='ctl00_ContentPlaceHolder1_ucDailyReportConsumer_cboSites_Input']").send_keys(Keys.ARROW_DOWN + Keys.ENTER)
-        # driver.save_screenshot('screen_shoot_3.png')
-        # driver.find_element_by_xpath("//input[@id='ctl00_ContentPlaceHolder1_ucDailyReportConsumer_cboSites_Input']").send_keys(Keys.ARROW_DOWN + Keys.ENTER)
-        # driver.save_screenshot('screen_shoot_4.png')
-
-        # # Extract the list of meters/locations from the dropdown
-        # # for li in driver.find_elements_by_css_selector("li.rcbItem.rcbTemplate"):
-        # ul = driver.find_element_by_xpath(
-        #     "//div[@id='ctl00_ContentPlaceHolder1_ucDailyReportConsumer_cboSites_DropDown']/div[2]/ul")
-        # for li in ul.find_elements_by_css_selector("li.rcbItem.rcbTemplate"):
-        #     cell = li.find_elements_by_tag_name("td")[0]
-        #     print(cell)
-        #     print(cell.text)
-        # # ctl00_ContentPlaceHolder1_ucDailyReportConsumer_cboSites_DropDown > div.rcbScroll.rcbWidth > ul > li:nth-child(1)
-        # # // *[ @ id = "ctl00_ContentPlaceHolder1_ucDailyReportConsumer_cboSites_DropDown"] / div[2] / ul / li[1]
-
-
-        # a = driver.find_element_by_xpath("//input[@id='ctl00_ContentPlaceHolder1_ucDailyReportConsumer_cboSites_ClientState']").get_attribute("value")
-        # print(a)
-        # print(type(a))
-        # if "102 LE QUOC HUNG" in a:
-        #     print("'102 LE QUOC HUNG' in a")
-
-
-        # driver.find_element_by_xpath(
-        #     "//div[@id='ctl00_ContentPlaceHolder1_ucDailyReportConsumer_cboSites_DropDown']")\
-        #     .send_keys(Keys.ARROW_DOWN + Keys.ARROW_DOWN + Keys.RETURN)
-
-        # driver.find_element_by_xpath(
-        #     "//div[@id='ctl00_ContentPlaceHolder1_ucDailyReportConsumer_cboSites_DropDown']//td[contains(text(), ' NVLinh_NLBang (ĐC)')]").click()
-
-        # driver.find_element_by_xpath(
-        #     "//input[@id='ctl00_ContentPlaceHolder1_ucDailyReportConsumer_cboSites_Input']").click()
-
-
-
-        # from selenium.webdriver.common.keys import Keys
-        # from selenium.webdriver import ActionChains
-        # # dropdown = driver.find_element_by_xpath(
-        # #     "//input[@id='ctl00_ContentPlaceHolder1_ucDailyReportConsumer_cboSites_Input']")
-        # selection = driver.find_element_by_xpath(
-        #     # "//li[@class='rcbItem']//tr[td='90 BEN VAN DON']")
-        #     "//div[@id='ctl00_ContentPlaceHolder1_ucDailyReportConsumer_cboSites_DropDown']//td[contains(text(), ' NVLinh_NLBang (ĐC)')]")
-        #     # "//div[@id='ctl00_ContentPlaceHolder1_ucDailyReportConsumer_cboSites_DropDown']//td[contains(text(), '90 BEN VAN DON')]")
-        # actions = ActionChains(driver).move_to_element(selection)
-        #                               .send_keys(Keys.ARROW_DOWN)\
-        #                               .send_keys(Keys.ARROW_DOWN)\
-        #                               .send_keys(Keys.ARROW_DOWN)
-        # .click()
-
-    # actions = ActionChains(driver).click(dropdown).send_keys('65 BEN VAN DON')
-    # # actions = ActionChains(driver).click(dropdown).click(selection).send_keys(Keys.ENTER)
-    #                               # .send_keys(Keys.ARROW_DOWN).send_keys(Keys.ARROW_UP)\
-    #                               # .send_keys(Keys.RETURN)
-    # actions.perform()
-
-    # from selenium.webdriver.common.keys import Keys
-    # element = driver.find_element_by_xpath("//input[@id='ctl00_ContentPlaceHolder1_ucDailyReportConsumer_dtmStart_dateInput']").send_keys(Keys.SHIFT + Keys.TAB)
-
-
-
-    # from selenium.webdriver.support.ui import Select
-    # select_obj = Select(driver.find_element_by_xpath("//div[@id='ctl00_ContentPlaceHolder1_ucDailyReportConsumer_cboSites_DropDown']"))
-    # select_obj.select_by_visible_text('400 NGUYEN VAN LINH')
-
-
-    # driver.find_element_by_xpath(
-    #     "//input[@id='ctl00_ContentPlaceHolder1_ucDailyReportConsumer_cboSites_ClientState']").click()
-
-    # driver.find_element_by_xpath(
-    #     "//div[@id='ctl00_ContentPlaceHolder1_ucDailyReportConsumer_cboSites_DropDown']/div[@class='rcbScroll rcbWidth']/td[text()=' 90 BEN VAN DON']").click()
-
-    # find_element_by_xpath("//select[@id='numReturnSelect']/option[text()='15000']").click()
-    # find_element_by_xpath("//select[@id='numReturnSelect']/option[@value='15000']").click()
-
-    # // *[ @ id = "ctl00_ContentPlaceHolder1_ucDailyReportConsumer_cboSites_DropDown"] / div[2] / ul / li[
-    #     7] / table / tbody / tr / td[1]
-
-    time.sleep(3)
-
-
-    # element = driver.find_element_by_xpath("//input[@id='ctl00_ContentPlaceHolder1_ucDailyReportConsumer_btnView_input']")
-    # element.click()
-    #
-    # wait.until(EC.element_to_be_clickable((By.ID, "ctl00_ContentPlaceHolder1_ucDailyReportConsumer_grv_ctl00")))
-
-
-    # driver.maximize_window()
-    driver.save_screenshot('screen_shoot.png')
     driver.close()
 
 
@@ -257,3 +265,11 @@ if __name__ == "__main__":
     main()
 else:
     print("This Python script is to be called as main(), not as the module ", __name__ + ".")
+
+
+"""
+90 BEN VAN DON
+    du lieu chi tiet trong Bang:
+        http://113.161.69.85:1802/Consumer/Logger/DetailTable.aspx?si=90 BEN VAN DON&dt=0
+    ko co Ap luc, toan 0.00, nhung trong trang logger co gia tri Ap luc Min Max trong Ngay
+"""
